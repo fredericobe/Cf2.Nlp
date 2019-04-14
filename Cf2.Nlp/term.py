d777c905a28047109e0321cbac8fb75b94b03e35
Frederico <fredericobe@gmail.com>
2019-04-14 00:12:19 -0300

    deleting old folders

--- Cf2.Nlp/term.py
+++
@@ -1,8 +0,0 @@
-class Term:
-    """ A structure to maintain a vlue and its stem """
-
-    value = ""
-    stem = "" 
-
-    def __repr__(self):
-        return str({'value': self.value, 'stem': self.stem})
